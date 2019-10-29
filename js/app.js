916008c43cda00a3232564c9645b46bab49d85bd
JayOkey <jayaokey@gmail.com>
2019-10-29 21:32:14 +1100

    First Commit

---
+++ js/app.js
@@ -0,0 +1,51 @@
+;(function(d){
+
+  "use strict";
+
+  var url = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes',
+      quotes = [],
+      blockquote = d.querySelector('blockquote'),
+      button = d.querySelector('button');
+
+  var getJSON = function(response) {
+    return (response.ok) ? response.json() : Promise.reject(response);
+  };
+
+  var rotateQuotes = function(quote) {
+    quotes.shift();
+    quotes.push(quote);
+  }
+
+  var checkQuote = function(data) {
+    if (quotes.indexOf(data[0]) === -1) {
+        quotes.push(data[0]);
+      if (quotes.length < 5) {
+        rotateQuotes(data[0]);
+      }
+    } else {
+      getQuote();
+    }
+    return data;
+  };
+
+  var renderQuote = function(data) {
+    blockquote.textContent = quotes[quotes.length - 1];
+  };
+
+  var renderError = function() {
+    console.log('Unable to get quote');
+  }
+
+  var getQuote = function() {
+    fetch(url)
+      .then(getJSON)
+      .then(checkQuote)
+      .then(renderQuote)
+      .catch(renderError);
+  };
+
+  getQuote();
+
+  button.addEventListener('click', getQuote, false);
+
+})(document);
